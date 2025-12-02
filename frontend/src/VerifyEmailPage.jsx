1324384b2fd8b92fda510693c74e05f0fff4243a
Vishesh Jangid <vishesh3126jangid@gmail.com>
2025-12-02 12:40:09 +0530

    Fix VerifyEmailPage useEffect warning

--- frontend/src/VerifyEmailPage.jsx
+++ frontend/src/VerifyEmailPage.jsx
@@ -17,6 +17,7 @@ export default function VerifyEmailPage() {
       setStatus('error');
       setError('No verification token provided');
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [searchParams]);
 
   async function handleVerification(token) {
